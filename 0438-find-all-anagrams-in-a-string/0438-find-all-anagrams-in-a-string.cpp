8fea17ac24ab858e94e101972c08b8c5868bc37c
ARPIT KUMAR DHURIYA <87499605+Arpit4288@users.noreply.github.com>
2023-02-06 14:31:00 +0530

    Time: 12 ms (84.68%), Space: 8.7 MB (44.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
-        vector<int> hash(26, 0);
-        vector<int> phash(26, 0);
-        int window = p.size();
-        int len = s.size();
-        if(len < window)
-        {
-            return ans;
+        vector<int> ans;  // for storing the indexes of starting index of every anagram
+        vector<int> freq1(26, 0);  // stores the frequency of every character 
+        for(int i = 0;i<p.length();i++){
+            freq1[p[i] - 'a']++; // Eg. a --> 0 : 1, b --> 1 : 1
         }
-        int left = 0,right = 0;
-        while(right < window)
-        {
-            phash[p[right] - 'a'] += 1;
-            hash[s[right] - 'a'] += 1;
-            right++;
-        }
-        right -=1;
-        while(right < len)
-        {
-            if(phash == hash)
-            {
-                ans.push_back(left);
+        int prev = 0, curr = 0;  // current window in 0
+        vector<int> freq2(26, 0);  // storing the window elements frequency
+        while(curr < s.length()){
+            freq2[s[curr] - 'a']++;  // taking the curr character ino the window
+            if(curr - prev + 1 > p.length()){  // if window size is greater than required
+                freq2[s[prev] - 'a']--; // then previous character through out and remove its freq from freq2
+                prev++;
             }
-            right+=1;
-            if(right != len)
-            {
-                hash[s[right] - 'a'] += 1;
+            if(curr - prev + 1 == p.length()){  // If window is full and window size is matched with p then we have to check is that window is matched with p or not if it match then store the starting index of that window into our ans
+                if(freq1 == freq2){
+                    // cout<<" Inside answering ";
+                    ans.emplace_back(prev);
+                }
             }
-            hash[s[left] - 'a'] -=1 ;
-            left += 1;
+            curr++;
         }
         return ans;
     }
