4d7506ded532fde5eb7d0e0484100e51744ac5a2
ARPIT KUMAR DHURIYA <87499605+Arpit4288@users.noreply.github.com>
2023-02-28 14:09:22 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.97%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& arr, int n) {
-        vector<int> ans;
-        for(int i = 0, j  = n; i < n && j < 2*n;i++,j++){
+        vector<int> ans; // initialze for storing the suffled elements of given array
+        for(int i = 0, j  = n; i < n && j < 2*n;i++,j++){ // first index starting from 0, and second index starting from middle of the array and at each iteration we have to move both by one, till the end
             ans.emplace_back(arr[i]);
             ans.emplace_back(arr[j]);
         }
