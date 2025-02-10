e2f064f4d3f8761192d0a2b67e8a9820dfe68659
ARPIT KUMAR DHURIYA <arpitkumardhuriya4288@gmail.com>
2025-02-10 10:22:44 +0530

    Time: 1 ms (70.09%), Space: 42.4 MB (35.51%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string ClearDigits(string s) {
+        if(s.Length < 2) return s;
+        for(int i = 1; i < s.Length; i++){
+            if(Char.IsDigit(s[i])){
+                s = s.Remove(i-1, 2);
+                return ClearDigits(s);
+            }
+        }
+        return s;
+    }
+}
