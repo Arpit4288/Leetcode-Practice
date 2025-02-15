d07883943a2714a97a4c148a13cb2f7bd7992616
ARPIT KUMAR DHURIYA <arpitkumardhuriya4288@gmail.com>
2025-02-15 16:35:21 +0530

    Time: 2 ms (100%), Space: 68.9 MB (28.88%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cs
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public IList<int> FindDuplicates(int[] arr) {
         IList<int> res = new List<int>();
-
         for(int i = 0; i < arr.Length; i++){
             int idx = Math.Abs(arr[i]) - 1;
             if(arr[idx] < 0){
