ec24669a4d8f94d3952ea99aa88c79e42d24ac44
ARPIT KUMAR DHURIYA <arpitkumardhuriya4288@gmail.com>
2025-02-15 16:50:02 +0530

    Time: 10 ms (6.58%), Space: 47.4 MB (46.71%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cs
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cs
@@ -1,13 +1,12 @@
 public class Solution {
-    public IList<int> TargetIndices(int[] nums, int target) {
+    public IList<int> TargetIndices(int[] arr, int target) {
         int smallerCount = 0, targetCount = 0;
 
-        for(int i = 0; i < nums.Length; i++){
-            if(nums[i] == target) targetCount++;
-            else if(nums[i] < target) smallerCount++;
+        for(int i = 0; i < arr.Length; i++){
+            if(arr[i] == target) targetCount++;
+            else if(arr[i] < target) smallerCount++;
         }
-        
-        if(targetCount == 0) return new List<int>();
-        else return Enumerable.Range(smallerCount, targetCount).ToList();
+        if(target == 0) return new List<int>();
+        return Enumerable.Range(smallerCount, targetCount).ToList();
     }
 }
