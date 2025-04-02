119657bf94749d32fb7ff9556080c94262176a22
ARPIT KUMAR DHURIYA <arpitkumardhuriya4288@gmail.com>
2025-04-02 19:57:24 +0530

    Time: 0 ms (100%), Space: 43.1 MB (35.19%) - LeetHub

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public long MaximumTripletValue(int[] arr) {
+        int n = arr.Length;
+        int[] storeRightMax = new int[n];
+        int[] storeLeftMax = new int[n];
+
+        int maxi = arr[n - 1];
+        for(int i = arr.Length - 1; i >= 0; i--){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+            storeRightMax[i] = maxi;
+            // Console.Write(maxi + " , " + mini);
+            // Console.WriteLine();
+        }
+
+        maxi = arr[0];
+        for(int i = 0; i < arr.Length; i++){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+            storeLeftMax[i] = maxi;
+            // Console.Write(maxi + " , " + mini);
+            // Console.WriteLine();
+        }
+
+        long ans = 0;
+        for(int i = 1; i < n - 1; i++){
+            long trip = (long)((long)storeLeftMax[i - 1] - (long)arr[i]) * (long)storeRightMax[i + 1];
+            // Console.WriteLine(trip + " >> Left MAX: " + storeLeftMax[i - 1] + " Right MAX: " + storeRightMax[i + 1]);
+            if(trip > ans) ans = trip;
+        }
+
+        return ans;
+    }
+}
