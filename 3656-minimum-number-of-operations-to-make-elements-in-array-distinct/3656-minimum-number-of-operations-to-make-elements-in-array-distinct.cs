074932956842eb5860f26377b5e08a8cefb9e883
ARPIT KUMAR DHURIYA <arpitkumardhuriya4288@gmail.com>
2025-04-08 14:09:46 +0530

    Time: 3 ms (71.79%), Space: 44.5 MB (78.2%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MinimumOperations(int[] arr) {
+        int n = arr.Length;
+        if(n == 1) return 0;
+        // if(n == 2) arr[0] == arr[1] ? 1 : 0;
+
+        HashSet<int> st = new HashSet<int>();
+
+        for(int i = n - 1; i >= 0; i--){
+            if(st.Contains(arr[i])){
+                int l = i + 1;
+                return (int)Math.Ceiling((i + 1) / 3.0);
+            }
+            else{
+                st.Add(arr[i]);
+            }
+        }
+
+        return 0;
+
+    }
+}
